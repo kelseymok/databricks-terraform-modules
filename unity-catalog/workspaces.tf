b4c5536275737bc610bd7fe3d0dcccc7521a4499
Kelsey Beyer <kelseymok@gmail.com>
2023-07-27 15:09:24 +0200

    using same type

--- unity-catalog/workspaces.tf
+++ unity-catalog/workspaces.tf
@@ -18,12 +18,12 @@ resource "databricks_metastore_data_access" "this" {
 
 locals {
   all_workspace_ids_generated = data.databricks_mws_workspaces.all.ids == null ? {} : data.databricks_mws_workspaces.all.ids
-  all_workspace_ids = length(var.workspace_ids_override) > 0 ? var.workspace_ids_override : local.all_workspace_ids_generated
+  all_workspace_ids =  length(var.workspace_ids_override) > 0 ? var.workspace_ids_override : [for i in values(local.all_workspace_ids_generated) : tostring(i)]
 }
 
 resource "databricks_metastore_assignment" "default_metastore" {
   provider             = databricks.workspace
-  for_each             = toset([for i in values(local.all_workspace_ids) : tostring(i)])
+  for_each             = toset(local.all_workspace_ids)
 #  for_each             = toset(var.databricks_workspace_ids)
   workspace_id         = each.key
   metastore_id         = databricks_metastore.this.id
