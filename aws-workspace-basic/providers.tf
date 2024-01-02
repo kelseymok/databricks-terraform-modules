7154e3fd37c71b2586fca3a52a3ab8d12b8929c2
Kelsey Beyer <kelseymok@gmail.com>
2024-01-02 14:44:39 +0100

    adding account_id to mws provider

--- aws-workspace-basic/providers.tf
+++ aws-workspace-basic/providers.tf
@@ -8,4 +8,5 @@ provider "databricks" {
   host     = "https://accounts.cloud.databricks.com"
   client_id = var.databricks_client_id
   client_secret = var.databricks_client_secret
+  account_id = var.databricks_account_id
 }
